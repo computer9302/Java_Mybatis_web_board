3c2c2a0e6a8c0751651cc34b0b685850f92c1e8c
computer9302 <computer9302@gmail.com>
2023-12-15 00:52:04 +0900

    Mybatis

---
+++ board/src/main/java/com/db/dao/BoardDao_batis.java
@@ -0,0 +1,59 @@
+package com.db.dao;
+
+import java.security.PublicKey;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.db.dto.BoardDto;
+
+public class BoardDao_batis extends SqlMapConfig{
+	
+	//게시글 전체 출력->목록
+	public List<BoardDto> selectAll(){
+		List<BoardDto> res = null;
+		SqlSession session = null;
+		
+		//객체 정보 가져와 session 만들어줌
+		try {
+			session = getSqlSessionFactory().openSession(true); //auto commit : true
+			
+			//mapper의 selectAll 결과값 list로 담아 res로 반환
+			res = session.selectList("db.board.selectAll");
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return res;
+	}
+	
+	//글 추가
+	public int insert(BoardDto dto) {
+		return 0;
+	}
+	
+	//글 수정
+	public int update(BoardDto dto) {
+		return 0;
+	}
+	
+	//글 삭제
+	public int delete(int bd_no) {
+			return 0;
+	}
+	
+	//글 여러개 삭제
+	public int multiDelete(String[] bd_no) {
+		return 0;
+	}
+	
+	public BoardDto selectOne(int bd_no) {
+		
+		BoardDto dto = null;
+		
+		return dto;
+	}
+	
+
+}
